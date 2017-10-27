1f2132e904a50561afa5045661be66b59863686e
milu-buet <milu.buet@gmail.com>
2017-10-27 12:13:58 -0500

    Q1 2

---
+++ code/generate_random_gnome.py
@@ -0,0 +1,22 @@
+#Q2
+
+from util import *
+
+
+
+def generateRandomGnome(n, data_file):
+	file = open(data_file , 'w')
+	file.write(random_dna(n) + '\n')
+	file.close()
+	return 0
+
+def loadRandomGnomeData(data_file):
+	file = open(data_file, 'r')
+	return file.read()
+	
+
+
+
+data_file = 'data/random_gnome.fasta'
+generateRandomGnome(1000,data_file)
+loadRandomGnomeData(data_file)
---
+++ code/generate_random_reads.py
@@ -0,0 +1,4 @@
+#Q3
+
+def generateRandomReads(reference_gnome, read_length, coverage):
+	pass
